f8837c78392e8a1365c82dd5601ba4003875a702
manjeetpixel <manjeetrai1219@gmail.com>
2026-05-31 16:32:49 +0530

    Refactor log file analyzer to use vectors for messages
    Refactor log file analysis to store messages in vectors for errors, warnings, fails, criticals, and unauthorized entries. Update output formatting for clarity.

--- log-file-analyzer.cpp
+++ log-file-analyzer.cpp
@@ -1,12 +1,19 @@
-
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <cctype>
+#include <vector>
 using namespace std;
 int main(){
     int error = 0;
+    vector<string>errorV;
     int warn = 0;
+    vector<string>warnV;
+    int fail = 0;
+    vector<string>failV;
+    int critical = 0;
+    vector<string>criticalV;
+    int un = 0;
+    vector<string>unV;
     cout<<"Enter file name: ";
     string file;
     getline(cin,file);
@@ -18,27 +25,79 @@ int main(){
     }
     //save in file
     ofstream report("report.txt");
-    report<<"Here is the report:"<<endl<<endl;
+    report<<"Here is the Report:"<<endl<<endl;
+    cout<<"Here is the Report:"<<endl<<endl;
     string line;
-    report<<"Errors: "<<endl;
     while(getline(output,line)){
         if(line.find("ERROR") != string::npos){
-            report<<"Error "<<error+1<<": ";
-            report<<line<<endl;
+            errorV.push_back(line);
             error++;
         }
-    }
-    output.clear(); //here i remove EOF 
-    output.seekg(0); // then get the line on top
-    report<<"Warnings: "<<endl;
-    while(getline(output,line)){
-                if(line.find("WARN") != string::npos){
-            report<<"warning "<<warn+1<<": ";
-            report<<line<<endl;
+        //warnings
+        if(line.find("WARN") != string::npos){
+            warnV.push_back(line);
             warn++;
         }
+        //fails
+        if(line.find("FAILED") != string::npos){
+            failV.push_back(line);
+            fail++;
+        }
+        //critical
+        if(line.find("CRITICAL") != string::npos){
+            criticalV.push_back(line);
+            critical++;
+        }
+        //UNauthrized
+        if(line.find("UNAUTHORIZED") != string::npos){
+            unV.push_back(line);
+            un++;
+        }
     }
-    report<<"There are total"<<endl<<"Errors: "<<error<<" "<<endl<<"Warnings: "<<warn<<" "<<endl;
+    if(error != 0){
+    report<<"ERRORS: "<<endl;
+    cout<<"ERRORS: "<<endl;
+    }
+
+    for(int i = 0 ; i < error; i++){
+        report<<"Error "<<i+1<<": "<<errorV[i]<<endl;
+        cout<<"\e[31m"<<"Error "<<i+1<<": "<<"\e[0m"<<errorV[i]<<endl;
+    }
+    if(warn != 0){
+    report<<"WARNINGS: "<<endl;
+    cout<<"WARNINGS: "<<endl;
+    }
+    for(int w = 0; w <warn; w++){
+        report<<"Warning "<<w+1<<": "<<warnV[w]<<endl;
+        cout<<"\e[33m"<<"Warning "<<w+1<<": "<<"\e[0m"<<warnV[w]<<endl;
+    }
+
+    if(fail != 0){
+    report<<"FAILs: "<<endl;
+    cout<<"FAILs: "<<endl;
+    }
+        for(int f = 0; f <fail; f++){
+        report<<"Failed: "<<f+1<<": "<<failV[f]<<endl;
+        cout<<"\e[35m"<<"failed: "<<f+1<<": "<<"\e[0m"<<failV[f]<<endl;
+    }
+    if(critical != 0){
+        report<<"Criticals: "<<endl;
+        cout<<"Criticals: "<<endl;
+    }
+        for(int c = 0; c <critical; c++){
+        report<<"Critical: "<<c+1<<": "<<criticalV[c]<<endl;
+        cout<<"\e[34m"<<"Critical: "<<c+1<<": "<<"\e[0m"<<criticalV[c]<<endl;
+    }
+    if(un !=0 ){
+        report<<"Unauthorizeds: "<<endl;
+        cout<<"Unauthorizeds: "<<endl;
+    }
+    for(int u = 0; u <un; u++){
+        report<<"Unauthorized: "<<u+1<<": "<<unV[u]<<endl;
+        cout<<"\e[36m"<<"Unauthorized: "<<u+1<<": "<<"\e[0m"<<unV[u]<<endl;
+    }
+    report<<"There are total"<<endl<<"Errors: "<<error<<" "<<endl<<"Warnings: "<<warn<<endl<<" "<<"Fails: "<<fail<<endl<<"Criticals: "<<critical<<endl<<"Unauthorizeds: "<<un<<endl;
+    cout<<"There are total"<<endl<<"Errors: "<<error<<" "<<endl<<"Warnings: "<<warn<<endl<<" "<<"Fails: "<<fail<<endl<<"Criticals: "<<critical<<endl<<"Unauthorizeds: "<<un<<endl;
     report.close();
     cout<<"File saved to report.txt";
     return 0;
