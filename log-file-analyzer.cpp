b935d249bdeaf337e2ec3300c413a120ded13918
Manjeet Kumar(moon) Rai <manjeetrai1219@gmail.com>
2026-09-07 16:13:30 +0530

    Refactor log file analysis using maps
    Refactored log file analysis to use a map for error types, simplifying the code structure and improving readability. Removed individual counters and vectors for error types.

--- log-file-analyzer.cpp
+++ log-file-analyzer.cpp
@@ -2,18 +2,20 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <algorithm>
+#include <map>
 using namespace std;
 int main(){
-    int error = 0;
-    vector<string>errorV;
-    int warn = 0;
-    vector<string>warnV;
-    int fail = 0;
-    vector<string>failV;
-    int critical = 0;
-    vector<string>criticalV;
-    int un = 0;
-    vector<string>unV;
+    // Creating a map for errortypes
+    map<string, vector<string>>logData;
+    // Creating color map
+    map<string, string> colorMap = {
+    {"ERROR", "\e[31m"},
+    {"WARN", "\e[33m"},
+    {"FAILED", "\e[35m"},
+    {"CRITICAL", "\e[34m"},
+    {"UNAUTHORIZED", "\e[36m"}
+    };
     cout<<"Enter file name: ";
     string file;
     getline(cin,file);
@@ -28,76 +30,45 @@ int main(){
     report<<"Here is the Report:"<<endl<<endl;
     cout<<"Here is the Report:"<<endl<<endl;
     string line;
+    string buffer;
+    // Checking through each line from file
     while(getline(output,line)){
-        if(line.find("ERROR") != string::npos || line.find("Error") != string::npos){
-            errorV.push_back(line);
-            error++;
+        buffer = line;
+        transform(buffer.begin(), buffer.end(), buffer.begin(), ::toupper);
+        if(buffer.find("ERROR") != string::npos){
+            logData["ERROR"].push_back(line);
         }
         //warnings
-        if(line.find("WARN") != string::npos || line.find("Warn") != string::npos){
-            warnV.push_back(line);
-            warn++;
+        if(buffer.find("WARN") != string::npos){
+            logData["WARN"].push_back(line);
         }
         //fails
-        if(line.find("FAILED") != string::npos || line.find("Failed") != string::npos ){
-            failV.push_back(line);
-            fail++;
+        if(buffer.find("FAILED") != string::npos){
+            logData["FAILED"].push_back(line);
         }
         //critical
-        if(line.find("CRITICAL") != string::npos || line.find("Critical") != string::npos){
-            criticalV.push_back(line);
-            critical++;
+        if(buffer.find("CRITICAL") != string::npos){
+            logData["CRITICAL"].push_back(line);
         }
-        //UNauthrized
-        if(line.find("UNAUTHORIZED") != string::npos || line.find("Unauthorized") != string::npos){
-            unV.push_back(line);
-            un++;
+        //Unauthorized
+        if(buffer.find("UNAUTHORIZED") != string::npos){
+            logData["UNAUTHORIZED"].push_back(line);
         }
     }
-    if(error != 0){
-    report<<"ERRORS: "<<endl;
-    cout<<"ERRORS: "<<endl;
+    // Reporting logData
+    for(auto &pair : logData){            
+    cout<<colorMap[pair.first] << pair.first << ": "<< "\e[0m" << endl;   
+    report << pair.first << ": "<<endl;
+    for(string line : pair.second){      
+        cout << line << endl;
+        report << line <<endl;
     }
-
-    for(int i = 0 ; i < error; i++){
-        report<<"Error "<<i+1<<": "<<errorV[i]<<endl;
-        cout<<"\e[31m"<<"Error "<<i+1<<": "<<"\e[0m"<<errorV[i]<<endl;
+    }   
+    // Looping for count all data
+    for (auto &pair : logData){
+        cout<<pair.first<<": "<<pair.second.size()<<endl;
+        report<<pair.first<<": "<<pair.second.size()<<endl;
     }
-    if(warn != 0){
-    report<<"WARNINGS: "<<endl;
-    cout<<"WARNINGS: "<<endl;
-    }
-    for(int w = 0; w <warn; w++){
-        report<<"Warning "<<w+1<<": "<<warnV[w]<<endl;
-        cout<<"\e[33m"<<"Warning "<<w+1<<": "<<"\e[0m"<<warnV[w]<<endl;
-    }
-
-    if(fail != 0){
-    report<<"FAILs: "<<endl;
-    cout<<"FAILs: "<<endl;
-    }
-        for(int f = 0; f <fail; f++){
-        report<<"Failed: "<<f+1<<": "<<failV[f]<<endl;
-        cout<<"\e[35m"<<"failed: "<<f+1<<": "<<"\e[0m"<<failV[f]<<endl;
-    }
-    if(critical != 0){
-        report<<"Criticals: "<<endl;
-        cout<<"Criticals: "<<endl;
-    }
-        for(int c = 0; c <critical; c++){
-        report<<"Critical: "<<c+1<<": "<<criticalV[c]<<endl;
-        cout<<"\e[34m"<<"Critical: "<<c+1<<": "<<"\e[0m"<<criticalV[c]<<endl;
-    }
-    if(un !=0 ){
-        report<<"Unauthorizeds: "<<endl;
-        cout<<"Unauthorizeds: "<<endl;
-    }
-    for(int u = 0; u <un; u++){
-        report<<"Unauthorized: "<<u+1<<": "<<unV[u]<<endl;
-        cout<<"\e[36m"<<"Unauthorized: "<<u+1<<": "<<"\e[0m"<<unV[u]<<endl;
-    }
-    report<<"There are total"<<endl<<"Errors: "<<error<<" "<<endl<<"Warnings: "<<warn<<endl<<" "<<"Fails: "<<fail<<endl<<"Criticals: "<<critical<<endl<<"Unauthorizeds: "<<un<<endl;
-    cout<<"There are total"<<endl<<"Errors: "<<error<<" "<<endl<<"Warnings: "<<warn<<endl<<" "<<"Fails: "<<fail<<endl<<"Criticals: "<<critical<<endl<<"Unauthorizeds: "<<un<<endl;
     report.close();
     cout<<"File saved to report.txt";
     return 0;
